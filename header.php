111323b347a2c98eb46d6344a97332d5520eecdb
Adam Age <Adamwage@hotmail.com>
2019-01-28 09:07:57 -0600

    Created global header and footer files to place in template files.

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <nav>
+            <h1>This is a header</h1>
+        </nav>
+    </body>
+</html>
