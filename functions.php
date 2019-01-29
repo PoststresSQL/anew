c56d7da5a957a3ee0a384d29d7c36920fbffb91d
Adam Age <Adamwage@hotmail.com>
2019-01-29 07:40:54 -0600

    Importing Foundation stylesheets by Zurb Foundation.

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
     function anew_files() {
+        wp_enqueue_style('foundation_styles', get_template_directory_uri() . '/styles/foundation.min.css');
         wp_enqueue_style('global_styles', get_stylesheet_uri());
     }
 
