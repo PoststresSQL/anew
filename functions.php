21ddd1a4b20c3ca3f16f34309980cfca76fc068a
Adam Age <Adamwage@hotmail.com>
2019-01-31 15:58:39 -0600

    Global header page created.

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
 
     function anew_files() {
+        wp_enqueue_style('navigation_styles', get_template_directory_uri() . '/styles/navigation.css');
+        wp_enqueue_style('fontawesome_icons', '//use.fontawesome.com/releases/v5.7.0/css/all.css');
         wp_enqueue_style('foundation_styles', get_template_directory_uri() . '/styles/foundation.min.css');
         wp_enqueue_style('global_styles', get_stylesheet_uri());
     }
--- header.php
+++ header.php
@@ -4,6 +4,56 @@
         <?php wp_head(); ?>
     </head>
     <body>
-        <nav>
-            <h1>This is a header</h1>
-        </nav>
+        <header>
+            <div class="grid-container grid-x">
+                <div class="large-2">
+                    <img src="wp-content/uploads/2019/01/anew-logo-final.png" />
+                </div>
+                <nav class="large-10 grid-x">
+                    <ul class="large-9 menu medium-horizontal">
+                        <li>
+                            <a href="/get-started" target="_blank" rel="noopener noreferrer">
+                                Get Started
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/faq" target="_blank" rel="noopener noreferrer">
+                                FAQ
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/blog" target="_blank" rel="noopener noreferrer">
+                                Blog
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/about-us" target="_blank" rel="noopener noreferrer">
+                                About Us
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="large-3 menu medium-horizontal align-right">
+                        <li>
+                            <a href="http://" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-facebook-square fa-lg"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-linkedin fa-lg"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-instagram fa-lg"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://" target="_blank" rel="noopener noreferrer">
+                                <i class="fab fa-twitter-square fa-lg"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
