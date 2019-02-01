d485d58eb8ea9b12977e4e8a514945b1e64e878c
Adam Age <Adamwage@hotmail.com>
2019-01-31 19:45:49 -0600

    Skeleton for Home page and Footer created. Needs to be styled.

--- footer.php
+++ footer.php
@@ -1,5 +1,30 @@
         <footer>
-            <p> This is a Footer </p>
+            <section class="grid-container">
+                <h2>
+                    Get beyond the struggle and live a life worth living.
+                </h2>
+                <a href="#">
+                    Start Today
+                </a>
+                <hr>
+                <ul class="menu">
+                    <li>
+                        <a href="#">
+                            Privacy Policy
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Terms &amp; Conditions
+                        </a>
+                    </li>
+                    <li>
+                        <h6>
+                            Anew &copy; 2018, All Rights Reserved.
+                        </h6>
+                    </li>
+                </ul>
+            </section>
         </footer>
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,79 @@
 <?php
-    echo 'Hello World!'
+
+    get_header();
+?>
+    <main>
+        <section class="grid-container">
+            <h1>
+                We are here to encourage &amp; challenge people
+            </h1>
+            <h3>
+                To live their best, most consistent, most sustainable life yet
+            </h3>
+            <a href="#">
+                Get Started
+            </a>
+        </section>
+        <section class="grid-container ">
+            <!-- approx 1074x816 pixels grid -->
+            <div>
+                <p>
+                    Welcome to Anew Accountibility and Lifestyle Coaching! Are you ready to start over? Get in shape? Eat better? Achieve the goals you’ve been waiting years to achieve? Change careers? Then you are at the right place. 
+                </p>
+            </div>
+            <div>
+                <h4>
+                    FAQ
+                </h4>
+                <h5>
+                    How Do I Get Started?
+                </h5>
+                <p>
+                    <a href="#">Fill out the form</a> and we will be in touch through the email address you provided.
+                </p>
+            </div>
+            <div>
+                <h5>
+                    How Much Does It Cost?
+                </h5>
+                <p>
+                    We have multiple plans available for your needs and budget. <a href="#">See Plans</a>
+                </p>
+            </div>
+        </section>
+        <section class="grid-container">
+            <h2>
+                What Are People Saying?
+            </h2>
+            <blockquote>
+                I didn't really know what to expect when I signed up with an accountability coach, but I knew that I was lacking some serious motivation when it came to my personal goals. Anne-Marie took the time to understand how I tick, what motivates me, and where I was seeing successes in my life specifically at work. <a href="#">Read More</a>
+            </blockquote>
+        </section>
+        <section class="grid-container">
+            <h2>
+                The Blogs
+            </h2>
+            <div>
+                <div>
+                    <h3>
+                        At The End of My Rope
+                    </h3>
+                    <p>
+                        I remember thinking that I didn't want to live anymore. The year was 2010 and I had just had it. I was drinking too much and I couldn't quit. I am sure 1 or 2 people had their... <a href="#">Read More</a>
+                    </p>
+                </div>
+                <div>
+                    <h3>
+                        At The End of My Rope
+                    </h3>
+                    <p>
+                        I remember thinking that I didn't want to live anymore. The year was 2010 and I had just had it. I was drinking too much and I couldn't quit. I am sure 1 or 2 people had their... <a href="#">Read More</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+    </main>
+
+<?php    
+    get_footer();
 ?>
