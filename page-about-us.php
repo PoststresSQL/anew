b464ff484fe1c158c51aaa150036f5d692ace3ac
Adam Age <Adamwage@hotmail.com>
2019-02-03 09:41:10 -0600

    About Us  template file created.

---
+++ page-about-us.php
@@ -0,0 +1,22 @@
+<?php
+    get_header();
+?>
+
+<main class="grid-container">
+    <h2>
+        About Us
+    </h2>
+    <p>
+        Anew Accountability and Lifestyle Coaching was birthed from an idea Anne-Marie Box had after closing out a 10 year Marketing career at a financial technology company.  Anne-Marie’s passion to help people realize their strength and ability in themselves fueled her desire to start the company. Anew is a company that embraces the idea that it’s never too late to start again. Anne-Marie firmly believes we are never too broken, too old, too undone, too unhealthy, too anything to begin the journey towards wholeness - body, mind, and soul.
+    </p>   
+    <p>
+        Anne-Marie’s experience with her lovable dysfunctional family, sexual abuse/assault, alcoholism/addiction, extremist behavior,  and PTSD amongst other things has helped shape the mission statement for Anew and where the company focuses its energy and what type of philosophy the company embodies for itself.
+    </p>
+    <p>
+        Anne-Marie lives in Central, Texas with her husband and their five year old son. Her hope is that Anew boosts her clients self awareness and self esteem; as well as help them build a sustaining and consistent lifestyle focused on health, wellness, and self care.
+    </p>
+</main>
+
+<?php
+    get_footer();
+?>
