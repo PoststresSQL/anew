e66415252aa36383c2608a057ade1b91643f3344
Adam Age <Adamwage@hotmail.com>
2019-02-08 11:37:51 -0600

    Restructured template page for single posts to include "Blog" as page title, featured image, and categories list.

--- single.php
+++ single.php
@@ -9,17 +9,43 @@
         $category_name = $category_object[0] -> name;
 ?>
 
-<main>
-    <div class="grid-container">
-        <h6>
-            <?php echo $category_name; ?>
-        </h6>
-        <h2>
-            <?php the_title(); ?>
-        </h2>
-        <?php
-            the_content();
-        ?>
+<main id="single-page">
+    <section class="page-title__section">
+        <div class="grid-container no-padding-bottom"> 
+            <h2 class="page-title">
+                Blog
+            </h2>
+        </div>
+    </section>
+    <div class="grid-container grid-x">
+        <section class="cell large-9">
+            <div class="post__image-container text-center">
+                <?php the_post_thumbnail( array( 450, 450 ) ); ?>
+            </div>
+            <h5 class="post__category">
+                <?php echo $category_name; ?>
+            </h5>
+            <h2 class="post__title">
+                <?php the_title(); ?>
+            </h2>
+            <?php
+                the_content();
+            ?>
+        </section>
+        <aside class="cell large-3"
+               id="categories-list">
+            <div class="categories-list__container">
+                <h5>Categories:</h5>
+                <ul class="no-bullet">
+                    <li>Career</li>
+                    <li>Fitness</li>
+                    <li>Life</li>
+                    <li>Mindset</li>
+                    <li>Nutrition & Diet</li>
+                    <li>Recovery</li>
+                </ul>
+            </div>
+        </aside>
     </div>
 </main>
 
