227e6d4188977faf7377098e70e724937f4a7b67
Adam Age <Adamwage@hotmail.com>
2019-02-08 11:58:14 -0600

    Removed the_content() since i'll be using custom design.

--- page-get-started.php
+++ page-get-started.php
@@ -2,17 +2,53 @@
     get_header();
 ?>
 
-<main>
-    <section>
+<main id="started-page">
+    <section class="page-title__section">
         <div class="grid-container">
-            <h2>
-                <?php the_title(); ?>
+            <h2 class="page-title">
+                <?php echo the_title(); ?>
             </h2>
-            <?php
-                while ( have_posts() ) : the_post(); 
-                    the_content();
-                endwhile; 
-            ?>
+        </div>
+    </section>
+    <section class="background-orange">
+        <div class="grid-container">
+            <h3>
+                Welcome to Anew Accountability and Lifestyle Coaching! Our Mission Statement revolves around our clients: To encourage and challenge people to live their best, most consistent, and most sustainable life yet.
+            </h3>
+            <h3>
+                Are you ready to...
+            </h3>
+            <div class="grid-container grid-x grid-padding-x">
+                <ul class="no-bullet cell large-6">
+                    <li>
+                        <h4>
+                            Start Over?
+                        </h4>
+                    </li>
+                    <li>
+                        <h4>
+                            Get in shape?
+                        </h4>
+                    </li>
+                    <li>
+                        <h4>
+                            Eat better?
+                        </h4>
+                    </li>
+                </ul>
+                <ul class="no-bullet cell large-6">
+                    <li>
+                        <h4>
+                            Achieve the goals you've been waiting years to achieve?
+                        </h4>
+                    </li>
+                    <li>
+                        <h4>
+                            Change careers?
+                        </h4>
+                    </li>
+                </ul>
+            </div>
         </div>
     </section>
     
