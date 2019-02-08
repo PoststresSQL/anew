ec566295ff926a320965d40e700aeabe91adb13d
Adam Age <Adamwage@hotmail.com>
2019-02-08 09:27:55 -0600

    Start Today CTA button leads to Get Started page.

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
         <footer>
-            <section class="grid-container text-center">
+            <div class="grid-container text-center">
                 <h2>
                     Get beyond the struggle and live a life worth living.
                 </h2>
-                <a href="#" class="button">
+                <a href="/get-started/" class="button">
                     Start Today
                 </a>
                 <hr>
@@ -26,7 +26,7 @@
                         </li>
                     </ul>
                 </div>
-            </section>
+            </div>
         </footer>
     </body>
 </html>
