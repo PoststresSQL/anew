104064d43bdbc268bba2fb00a2cf39bde2ee5528
Adam Age <Adamwage@hotmail.com>
2019-02-09 11:55:08 -0600

    Added padding between the featured posts.

--- page-faq.php
+++ page-faq.php
@@ -26,22 +26,21 @@
             <h2>
                 The Blog
             </h2>
-            <div class="grid-x">
+            <div class="grid-x grid-padding-x">
                 <?php
                     $args = array(
                         'numberposts' => 2
                     );
-
                     $featured_posts = get_posts($args);
                     foreach ($featured_posts as $featured_post) {
                         $post_excerpt = substr($featured_post -> post_content, 0, 204);
                 ?>
                 <div class="cell large-6">
-                    <h3>
+                    <h4>
                         <?php echo $featured_post -> post_title; ?>
-                    </h3>
+                    </h4>
                     <p>
-                        <?php echo $post_excerpt; ?>...
+                        <?php echo $post_excerpt, '...'; ?>
                         <a href="<?php echo the_permalink(); ?>">Read More</a>
                     </p>
                 </div>
--- page-get-started.php
+++ page-get-started.php
@@ -10,45 +10,68 @@
             </h2>
         </div>
     </section>
+
     <section class="background-orange">
         <div class="grid-container">
-            <h3>
+            <h4>
                 Welcome to Anew Accountability and Lifestyle Coaching! Our Mission Statement revolves around our clients: To encourage and challenge people to live their best, most consistent, and most sustainable life yet.
-            </h3>
-            <h3>
+            </h4>
+            <h4>
                 Are you ready to...
-            </h3>
-            <div class="grid-container grid-x grid-padding-x">
+            </h4>
+            <div class="grid-container grid-x grid-padding-x"
+                 id="get-started__list">
                 <ul class="no-bullet cell large-6">
                     <li>
-                        <h4>
+                        <h5>
+                            <i class="far fa-compass fa-lg"></i>
                             Start Over?
-                        </h4>
+                        </h5>
                     </li>
                     <li>
-                        <h4>
+                        <h5>
+                            <i class="far fa-heart fa-lg"></i>
                             Get in shape?
-                        </h4>
+                        </h5>
                     </li>
                     <li>
-                        <h4>
+                        <h5>
+                            <i class="far fa-lemon fa-lg"></i>
                             Eat better?
-                        </h4>
+                        </h5>
                     </li>
                 </ul>
                 <ul class="no-bullet cell large-6">
                     <li>
-                        <h4>
+                        <h5>
+                            <i class="far fa-star fa-lg"></i>
                             Achieve the goals you've been waiting years to achieve?
-                        </h4>
+                        </h5>
                     </li>
                     <li>
-                        <h4>
+                        <h5>
+                            <i class="far fa-building fa-lg"></i>
                             Change careers?
-                        </h4>
+                        </h5>
                     </li>
                 </ul>
             </div>
+        </div>
+    </section>
+
+    <section id="user-form__section">
+        <div class="grid-container">
+            <div class="form-text">
+                <h4>
+                    Fill out the form
+                </h4>
+                <h5>
+                    Are you ready to get started? Do you want to achieve measurable results now? Complete the form below to schedule your session with your Anew Coach.
+                </h5>
+            </div>
+            <?php
+                echo do_shortcode('[wpforms id="45" title="false" description="false"]');
+            ?>
         </div>
     </section>
     
@@ -57,22 +80,21 @@
             <h2>
                 The Blog
             </h2>
-            <div class="grid-x">
+            <div class="grid-x grid-padding-x">
                 <?php
                     $args = array(
                         'numberposts' => 2
                     );
-
                     $featured_posts = get_posts($args);
                     foreach ($featured_posts as $featured_post) {
                         $post_excerpt = substr($featured_post -> post_content, 0, 204);
                 ?>
                 <div class="cell large-6">
-                    <h3>
+                    <h4>
                         <?php echo $featured_post -> post_title; ?>
-                    </h3>
+                    </h4>
                     <p>
-                        <?php echo $post_excerpt; ?>...
+                        <?php echo $post_excerpt, '...'; ?>
                         <a href="<?php echo the_permalink(); ?>">Read More</a>
                     </p>
                 </div>
