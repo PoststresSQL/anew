3924d310af381312f6e06bb1251881eb60824472
Adam Age <Adamwage@hotmail.com>
2019-02-09 12:37:20 -0600

    Fixed how each page was loading the_permalink() differently by grabbing featured post links by ID.

--- page-about-us.php
+++ page-about-us.php
@@ -21,34 +21,35 @@
     </section>
 
     <section id="featured-posts">
-            <div class="grid-container">
-                <h2>
-                    The Blog
-                </h2>
-                <div class="grid-x grid-padding-x">
-                    <?php
-                        $args = array(
-                            'numberposts' => 2
-                        );
-                        $featured_posts = get_posts($args);
-                        foreach ($featured_posts as $featured_post) {
-                            $post_excerpt = substr($featured_post -> post_content, 0, 204);
-                    ?>
-                    <div class="cell large-6">
-                        <h4>
-                            <?php echo $featured_post -> post_title; ?>
-                        </h4>
-                        <p>
-                            <?php echo $post_excerpt, '...'; ?>
-                            <a href="<?php echo the_permalink(); ?>">Read More</a>
-                        </p>
-                    </div>
-                    <?php
-                        }
-                    ?>
+        <div class="grid-container">
+            <h2>
+                The Blog
+            </h2>
+            <div class="grid-x grid-padding-x">
+                <?php
+                    $args = array(
+                        'numberposts' => 2
+                    );
+                    $featured_posts = get_posts($args);
+                    foreach ($featured_posts as $featured_post) {
+                        $post_excerpt = substr($featured_post -> post_content, 0, 204);
+                        $post_link = get_permalink($featured_post -> ID);
+                ?>
+                <div class="cell large-6">
+                    <h4>
+                        <?php echo $featured_post -> post_title; ?>
+                    </h4>
+                    <p>
+                        <?php echo $post_excerpt, '...'; ?>
+                        <a href="<?php echo $post_link; ?>">Read More</a>
+                    </p>
                 </div>
+                <?php
+                    }
+                ?>
             </div>
-        </section>
+        </div>
+    </section>
 </main>
 
 <?php
